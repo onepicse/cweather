4e7a1c0b784bf00470ca962624762644e11f50be
陈成 <840764528@qq.com>
2018-01-24 15:27:03 +0800

    移除vue-router，小部分代码添加

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,77 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" class="cold">
+    <div class="bottom-nav">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  beforeCreat () {
+
+  },
+  created () {
+
+  },
+  beforeMount () {
+
+  },
+  Mounted () {
+
+  },
+  beforeDestroy () {
+
+  },
+  destroyed () {
+
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+html,body {
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
+body{
+  -webkit-tap-highlight-color:rgba(0, 0, 0, 0);line-height:12px;-webkit-user-select:none;word-break:break-all;word-wrap:break-word;font-family: Helvetica, Tahoma, Arial, "PingFang SC", "Hiragino Sans GB", "Heiti SC", "Source Han Sans SC", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
+}
+#app{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .bottom-nav{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 1.76rem;
+  }
+  &.sunny{
+    background-image: linear-gradient( 0deg, #FCCF31 10%, #F55555 100%);
+    .bottom-nav{
+      background-color: #F55555;
+    }
+  }
+  &.cold{
+    background-image: linear-gradient( 0deg, #ABDCFF 10%, #0396FF 100%);
+    .bottom-nav{
+      background-color: #0396FF;
+    }
+  }
+}
+
 </style>
